d83212a10d896b93e37fac9762f4cb0a035a8e83
nno3onn <nno3onn@naver.com>
2021-09-11 01:57:15 +0900

    edit code react hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,31 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
-class App extends Component {
-  state = {
-    diarys: [],
-  };
+const App = () => {
+  const [diary, setDiary] = useState();
 
-  async componentDidMount() {
+  useEffect(async () => {
     try {
       const res = await fetch("http://127.0.0.1:8000/diary/");
-      const diarys = await res.json();
-      this.setState({
-        diarys,
-      });
-    } catch (e) {
-      console.log(e);
+      const diary = await res.json();
+      console.log(diary);
+      setDiary(diary);
+    } catch (error) {
+      return console.error(error);
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        {"안녕하세요"}
-        {/* {this.state.diarys.map((item) => (
-          <div key={item.id}>
-            <h1>{item.title}</h1>
-            <span>{item.descriptions}</span>
-          </div>
-        ))} */}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {"안녕하세요"}
+      {diary}
+      {/* {diary.map((item) => (
+        <div key={item.id}>
+          <h1>{item.title}</h1>
+          <span>{item.descriptions}</span>
+        </div>
+      ))} */}
+    </div>
+  );
+};
 
 export default App;
