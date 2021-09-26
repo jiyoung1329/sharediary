2f3fbc16e09a9bfcaa7e976bb5206c7bb379b304
nno3onn <nno3onn@naver.com>
2021-09-26 22:21:39 +0900

    210926 login page UI

--- frontend/src/sign/login.js
+++ frontend/src/sign/login.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./login.css";
 
 const Login = () => {
   return (
-    <div>
-      <h2>login page</h2>
+    <div className="sign-wrapper">
+      <div className="signIn-wrapper">
+        <h1 className="signIn-title">Sign In</h1>
+        <input className="signIn-input" type="text" placeholder="ID" />
+        <input
+          className="signIn-input"
+          type="password"
+          placeholder="PASSWORD"
+        />
+        <button className="signIn-btn">Sign up</button>
+      </div>
+      <div className="signUp-wrapper">
+        <h1 className="signUp-title">안녕하세요 !</h1>
+        <p className="signUp-detail">나만의 다이어리를 작성해보아요.</p>
+        <button className="signUp-btn">Sign up</button>
+      </div>
     </div>
   );
 };
--- frontend/src/sign/sign.js
+++ frontend/src/sign/sign.js
@@ -3,7 +3,7 @@ import React from "react";
 const Sign = () => {
   return (
     <div>
-      <h2>signup page</h2>
+      <h2>signin page</h2>
     </div>
   );
 };
