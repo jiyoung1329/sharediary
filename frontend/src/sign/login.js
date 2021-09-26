09ac8b9bda9a4c3e5ba5e020ff2def4bdd4a0b77
nno3onn <nno3onn@naver.com>
2021-09-26 22:31:45 +0900

    210926 - add Routing in Login page

--- frontend/src/sign/login.js
+++ frontend/src/sign/login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./login.css";
 
 const Login = () => {
@@ -17,7 +18,9 @@ const Login = () => {
       <div className="signUp-wrapper">
         <h1 className="signUp-title">안녕하세요 !</h1>
         <p className="signUp-detail">나만의 다이어리를 작성해보아요.</p>
-        <button className="signUp-btn">Sign up</button>
+        <Link to="/signUp">
+          <button className="signUp-btn">Sign up</button>
+        </Link>
       </div>
     </div>
   );
