a29c7973d3df18d6cf3e2dbea4e722d1805b58e6
jykim <gg>
2021-09-26 22:26:56 +0900

    210926 - make signup page

--- frontend/src/sign/sign.js
+++ frontend/src/sign/sign.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "src/sign/sign.css";
 
 const Sign = () => {
   return (
-    <div>
-      <h2>signup page</h2>
+    <div class="signup">
+
+      <h2 class="title">Sign Up</h2>
+      
+      <input class="id" 
+             placeholder="ID" />
+
+      <input class="password"
+             placeholder="PASSWORD"
+             type="password" />
+
+      <input class="passwordCheck"
+             placeholder="PASSWORD CHECK"
+             type="password" />
+
+      <button class="signupButton">Sign up</button>
     </div>
   );
 };
