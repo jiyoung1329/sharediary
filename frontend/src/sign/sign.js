c8636e52af4762ce12ac78ceeff1ae17679d0ef5
nno3onn <nno3onn@naver.com>
2021-09-26 22:47:39 +0900

    210926 - login & signup page

--- frontend/src/sign/sign.js
+++ frontend/src/sign/sign.js
@@ -4,24 +4,20 @@ import "src/sign/sign.css";
 const Sign = () => {
   return (
     <div class="signup">
+      <h1 class="title">Sign up</h1>
 
-      <h2 class="title">Sign Up</h2>
-      
-      <input class="id" 
-             placeholder="ID" />
+      <input class="id" placeholder="ID" />
 
-      <input class="password"
-             placeholder="PASSWORD"
-             type="password" />
+      <input class="password" placeholder="PASSWORD" type="password" />
 
-      <input class="passwordCheck"
-             placeholder="PASSWORD CHECK"
-             type="password" />
+      <input
+        class="passwordCheck"
+        placeholder="PASSWORD CHECK"
+        type="password"
+      />
 
       <button class="signupButton">Sign up</button>
-
     </div>
-
   );
 };
 export default Sign;
