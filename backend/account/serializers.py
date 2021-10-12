9460e966697b9b8c3f0f4e45e1064daedbf085df
jykim <jiyoung112837@gmail.com>
2021-10-12 23:25:20 +0900

    211012 - make API of account(signup, login)

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id')
+
 class RegisterSerializer(serializers.ModelSerializer) : 
     class Meta:
         model = User
